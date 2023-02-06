b8c3f9ae423f7c407b968594d721f0644441978c
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2023-02-06 23:55:37 +0530

    Create parity_of_couples_geeks.cpp

---
+++ Week 1/parity_of_couples_geeks.cpp
@@ -0,0 +1,9 @@
+class Solution{
+    public:
+    int findSingle(int N, int arr[]){
+        // code here
+        int ans = 0;
+        for(int i = 0 ;i < N ; i ++) ans ^= arr[i];
+        return ans;
+    }
+};
