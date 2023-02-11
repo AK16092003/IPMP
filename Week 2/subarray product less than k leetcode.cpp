ec4fa0b01fbfbc1c64a717a2081193379d407f80
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2023-02-11 23:03:07 +0530

    Create subarray product less than k leetcode.cpp

---
+++ Week 2/subarray product less than k leetcode.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int prod = 1;
+        if(k <= 1) return 0;
+        int n = nums.size();
+        int ans = 0;
+        int l = 0;
+        for(int r = 0 ; r < n ; r ++)
+        {
+            prod *= nums[r];
+            while(prod >= k )
+            {
+                prod /= nums[l];
+                l++;
+            }
+
+            ans += (r - l + 1);
+        }
+        return ans;
+    }
+};
