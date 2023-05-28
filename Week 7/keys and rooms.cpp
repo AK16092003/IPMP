a7bc9d9906fd102bf95750cbbef3273b6f39a996
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2023-05-28 14:29:45 +0530

    Create keys and rooms.cpp

---
+++ Week 7/keys and rooms.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<int> visited(n , 0);
+
+        queue<int> q;
+        q.push(0);
+        int count = 0;
+        visited[0] = 1;
+        count++;
+
+        while(q.size())
+        {
+            int u = q.front();
+            q.pop();
+            for(int v : rooms[u])
+            {
+                if(!visited[v])
+                {
+                    visited[v] = 1;
+                    q.push(v);
+                    count++;
+                }
+            }
+        }
+
+        return count == n;
+    }
+};
