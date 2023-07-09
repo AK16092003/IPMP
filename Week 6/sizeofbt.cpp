fce9a6a37cd655274669a8756a52825e1887d2af
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2023-07-09 23:00:34 +0530

    Create sizeofbt.cpp

---
+++ Week 6/sizeofbt.cpp
@@ -0,0 +1,7 @@
+/* Computes the number of nodes in a tree. */
+int getSize(Node* node)
+{
+   // Your code here
+   if(node == NULL) return 0;
+   return getSize(node->left) + getSize(node->right)+1;
+}
